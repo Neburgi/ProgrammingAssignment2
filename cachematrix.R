971642b009da30f3b23e103622a4d6886d9193fa
Neburgi <rubengi7@hotmail.com>
2014-08-24 11:49:23 +0200

    Revert "Revert "A few more comments on what to do""
    This reverts commit 769d233d09c66d3a316f8e5716ba122d9bbc8bc4.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,48 @@
-## [Put comments here that describe what your functions do]
+## Put comments here that give an overall description of what your
+## functions do
+
+## This function creates a "special matrix" thay cache its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  a <- NULL
+  set <- function(y) {
+    x <<- y
+    a <<- NULL
+  }
+  ## sets matrix and resets cached inverse
+  
+  get <- function() {
+    x
+  }
+  ## it returns matrix
+  
+  setSolve <- function(solve) {
+    a <<- solve
+  }
+  ## it saves solve value
+  
+  getSolve <- function() {
+    a
+  }
+  ## it returns cache inverse value
+  list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
+  
 }
 
 
+## This function is to get the inversed matrix from a special object created previously (makeCacheMatrix).
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  a <- x$getSolve()
+  if(!is.null(a)) {
+    message("getting cached data")
+    return(a)
+  }
+  ##Takes the object of that type as an argument 'x', checks if the inverse value is already cached, and if it is returns the cached value. If not, next step is taken
+  
+  data <- x$get()
+  a <- solve(data, ...)
+  x$setSolve(a)
+  a
+  ##This function calculates the inverse for the matrix saved in the 'x', saves it into 'x' cache using method 'setSolve'and returns the result.
 }
